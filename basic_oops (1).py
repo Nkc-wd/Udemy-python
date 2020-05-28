370bd7b6054ec4c2ffa90ef355ee0aa4848011a2
Niraj Chaurasia <60880978+Nkc-wd@users.noreply.github.com>
2020-05-28 12:54:47 +0530

    Basic oops

---
+++ basic_oops (1).py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[24]:
+
+
+class Dog:
+    def __init__(this,breed,origin,name):
+        this.breed=breed
+        this.origin=origin
+        this.name=name
+    def bark(this,time):
+        print("woof woof "*time)
+    def show(this,pet):
+        print("Hello",pet)
+
+
+# In[28]:
+
+
+dog=Dog("Deshi","nepal","kaalu")
+dog.bark(3)
+dog.show("kaalu")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
